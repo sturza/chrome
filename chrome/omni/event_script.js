160de7139de37c42024e0df81942542b87770224
sturza <sturza@me.com>
2017-01-03 07:00:53 +0200

    minor update

--- chrome/omni/event_script.js
+++ chrome/omni/event_script.js
@@ -12,8 +12,23 @@ var suggestResultOne = {
 var suggestResults = [suggestResultOne];
 var searchService = "https://www.google.com";
 
-searchService += "search?q=chrome+extensions+developers+";
+searchService += "/search?q=chrome+extensions+developers+";
 function CreateWindow(url) {
     var windowCreateData = {"url": ""};
-    
+    windowCreateData.url = url;
+    chrome.windows.create(windowCreateData);
 }
+
+chrome.omnibox.onInputStarted.addListener(function(){
+    console.log("<InputStarted>");
+});
+
+chrome.omnibox.onInputChanged.addListener(function(text, suggest){
+    console.log("<InputChanged> Text: " + text);
+    suggest(getSuggestResults(text));
+});
+
+chrome.omnibox.onInputEntered.addListener(function(text, disposition){
+    console.log("<InputEntered> Text: " + text);
+    CreateWindow(searchService + text);
+});
