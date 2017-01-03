f2371cee4bc58b19c9479e8f7565a63478f8eb96
sturza <sturza@me.com>
2017-01-03 05:34:40 +0200

    yay

---
+++ chrome/omni/event_script.js
@@ -0,0 +1,19 @@
+var ON_INPUT_ENTERED_DISPOSITION = {
+    "CURRENT_TAB": "currentTab",
+    "NEW_FOREGROUND_TAB": "newForegroundTab",
+    "NEW_BACKGROUND_TAB": "newBackgroundTab"
+};
+
+var suggestResultOne = {
+    "content": "Some content",
+    "description": "Description"
+};
+
+var suggestResults = [suggestResultOne];
+var searchService = "https://www.google.com";
+
+searchService += "search?q=chrome+extensions+developers+";
+function CreateWindow(url) {
+    var windowCreateData = {"url": ""};
+    
+}
